6b0e63a94a7b3f9944d749a66551e28bf7757d73
pataiadam <pataiadam@gmail.com>
2015-08-19 01:28:11 +0200

    Create some tests

--- test/index.js
+++ test/index.js
@@ -1,3 +1,26 @@
-/**
- * Created by Adam on 2015.08.19..
- */
+var should = require('chai').should(),
+    d = require('../index'),
+    norm = d.norm;
+
+describe('#norm', function() {
+    var vector1 = [0, 0];
+    var vector2 = [1, 1];
+    it('should return correct euclidean distance', function() {
+        norm(vector1, vector2).should.equal(Math.sqrt(2));
+        norm(vector1, vector2, 2).should.equal(Math.sqrt(2));
+        norm(vector2).should.equal(Math.sqrt(2));
+        norm(vector2, 2).should.equal(Math.sqrt(2));
+    });
+
+    it('should return 1-norm (Taxicab norm or Manhattan norm)', function() {
+        norm(vector1, vector2, 1).should.equal(2);
+        norm(vector2, 1).should.equal(2);
+    });
+
+    it('should return inf-norm (Maximum norm)', function() {
+        norm(vector1, vector2, Number.POSITIVE_INFINITY).should.equal(1);
+        norm(vector1, vector2, 'inf').should.equal(1)
+        norm(vector2, Number.POSITIVE_INFINITY).should.equal(1);
+        norm(vector2, 'inf').should.equal(1);
+    });
+});
